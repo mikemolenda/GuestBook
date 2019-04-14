31ea92162a37585580adc7f3d9b08146170c5b3b
Mike Molenda <molenda.mike@gmail.com>
2019-04-14 11:32:59 -0500

    Add Application test

---
+++ src/test/java/com/mikemolenda/guestbook/GuestbookApplicationTest.java
@@ -0,0 +1,16 @@
+package com.mikemolenda.guestbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuestbookApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
